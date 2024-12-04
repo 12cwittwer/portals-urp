3781b20be39c11894d8da0249a9ab20745dd2926
Christian Wittwer <christianwittwer@Christians-MacBook-Air.local>
2024-12-04 15:48:57 -0700

    Functional wall running

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float movementSpeed;
+    private float movementSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float wallrunSpeed;
+
     public Transform orientation;
     public float groundDrag;
 
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
+    [Header("Jumping")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
@@ -22,20 +32,38 @@ public class PlayerMovement : MonoBehaviour
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
-    bool grounded;
+    public bool grounded;
+
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
 
     [Header("Keybinds")]
 
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
 
     Rigidbody rb;
+
+    public MovementState state;
+    public enum MovementState {
+        walking,
+        sprinting, 
+        crouching,
+        wallrunning,
+        air,
+    }
+
+    public bool wallrunning;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         readyToJump = true;
+        startYScale = transform.localScale.y;
     }
 
     private void MyInput() {
@@ -47,36 +75,67 @@ public class PlayerMovement : MonoBehaviour
             Jump();
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+
+        if (Input.GetKey(crouchKey)) {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+        if (Input.GetKeyUp(crouchKey)) {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-        MyInput();
-        SpeedControl();
-        if (grounded) 
-            rb.drag = groundDrag;
-        else
-            rb.drag = 0;
+private void Update()
+{
+    // Ground check
+    grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
 
-        
-    }
+    MyInput();
+    SpeedControl();
+    StateHandler();
+
+    // Adjust drag based on state
+    if (grounded)
+        rb.drag = groundDrag;
+    else
+        rb.drag = 1f; // Slight drag in air
+}
 
     private void FixedUpdate() {
         MovePlayer();
     }
 
-    private void MovePlayer() {
-        // Calculate Movement Direction
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+private void MovePlayer()
+{
+    // Calculate movement direction
+    moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        if (grounded) {
-            rb.AddForce(moveDirection.normalized * movementSpeed * 10f, ForceMode.Force);
-        } else {
-            rb.AddForce(moveDirection.normalized * movementSpeed * 10f * airMultiplier, ForceMode.Force);
+    if (OnSlope() && grounded)
+    {
+        // Move on slopes
+        rb.AddForce(GetSlopeMoveDirection() * movementSpeed * 10f, ForceMode.Force);
+    }
+    else if (grounded)
+    {
+        // Regular ground movement
+        rb.AddForce(moveDirection.normalized * movementSpeed * 10f, ForceMode.Force);
+    }
+    else
+    {
+        // Air movement
+        rb.AddForce(moveDirection.normalized * movementSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        // Limit air velocity
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (flatVel.magnitude > movementSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * movementSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
+}
+
 
     private void SpeedControl() {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
@@ -88,13 +147,57 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    private void Jump() {
-        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+    private void Jump()
+    {
+        // Reset vertical velocity only if descending
+        if (rb.velocity.y < 0)
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
 
+        // Apply jump force
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
     private void ResetJump() {
         readyToJump = true;
+    }
+    private void StateHandler()
+    {
+        if (wallrunning)
+        {
+            state = MovementState.wallrunning;
+            movementSpeed = wallrunSpeed;
+        }
+        else if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            movementSpeed = crouchSpeed;
+        }
+        else if (grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            movementSpeed = sprintSpeed;
+        }
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            movementSpeed = walkSpeed;
+        }
+        else
+        {
+            state = MovementState.air;
+            movementSpeed = walkSpeed; // Maintain a base speed in air
+        }
+    }
+
+    private bool OnSlope() {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f)) {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection() {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
     }
 }
---
+++ Assets/Scripts/WallRunning.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRunning : MonoBehaviour
+{
+    [Header("Wallrunning")]
+    public LayerMask whatIsGround;
+    public LayerMask whatIsWall;
+    public float wallRunForce;
+    public float wallJumpUpForce;
+    public float wallJumpSideForce;
+    public float maxWallRunTime;
+    private float wallRunTimer;
+
+    [Header("Attachment")]
+    public float reattachAngleThreshold = 15;
+    private Vector3 lastWallNormal;
+    public bool recentlyDetached = false;
+
+    [Header("Input")]
+    public KeyCode jumpKey = KeyCode.Space;
+    private float horizontalInput;
+    private float verticalInput;
+
+    [Header("Exiting")]
+    private bool exitingWall;
+    public float exitWallTime;
+    private float exitWallTimer;
+
+    [Header("Detection")]
+    public float wallCheckDistance;
+    public float minJumpHeight;
+    private RaycastHit leftWallHit;
+    private RaycastHit rightWallHit;
+    private bool wallRight;
+    private bool wallLeft;
+
+
+    [Header("References")]
+    public Transform orientation;
+    private PlayerMovement pm;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForWall();
+        StateMachine();
+    }
+
+    void FixedUpdate() {
+        if (pm.wallrunning) {
+            WallRunningMovement();
+        }
+    }
+
+    private void CheckForWall() {
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallHit, wallCheckDistance, whatIsWall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallHit, wallCheckDistance, whatIsWall);
+    }
+
+    private bool AboveGround() {
+        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
+    }
+
+    private void StateMachine() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (pm.grounded) {
+            recentlyDetached = false;
+        }
+
+        if ((wallLeft || wallRight) && verticalInput > 0 && AboveGround() && !exitingWall && !CloseAngle()) {
+            //Start wallrunning here
+            if (!pm.wallrunning) {
+                StartWallRun();
+            }
+            if (wallRunTimer > 0)
+                wallRunTimer -= Time.deltaTime;
+
+            if (wallRunTimer <= 0 && pm.wallrunning) {
+                exitingWall = true;
+                exitWallTimer = exitWallTime;
+            }
+
+            if (Input.GetKeyDown(jumpKey)) {
+                WallJump();
+            }
+        }
+        else if (exitingWall) {
+            if (pm.wallrunning)
+                StopWallRun();
+
+            if (exitWallTimer > 0) {
+                exitWallTimer -= Time.deltaTime; 
+            }
+
+            if (exitWallTimer <= 0)
+                exitingWall = false;
+        }
+        else {
+            if (pm.wallrunning) {
+                StopWallRun();
+            }
+        }
+    }
+
+    private void StartWallRun() {
+        pm.wallrunning = true;
+        wallRunTimer = maxWallRunTime;
+        recentlyDetached = false;
+    }
+
+    private bool CloseAngle() {
+        if (!recentlyDetached)
+            return false;
+        
+        Vector3 wallNormal = wallRight ? rightWallHit.normal : leftWallHit.normal;
+
+        float angle = Vector3.Angle(lastWallNormal, wallNormal);
+
+        return angle < reattachAngleThreshold;
+    }
+
+    private void WallRunningMovement() {
+        rb.useGravity = false;
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+
+        Vector3 wallNormal = wallRight ? rightWallHit.normal : leftWallHit.normal;
+
+        lastWallNormal = wallNormal;
+
+        Vector3 wallForward = Vector3.Cross(wallNormal, transform.up);
+
+        if ((orientation.forward - wallForward).magnitude > (orientation.forward - -wallForward).magnitude) {
+            wallForward = -wallForward;
+        }
+
+        rb.AddForce(wallForward * wallRunForce, ForceMode.Force);
+
+        if (!(wallLeft && horizontalInput > 0) && !(wallRight && horizontalInput < 0)) {
+            rb.AddForce(-wallNormal * 100, ForceMode.Force);
+        }
+    }
+    private void StopWallRun() {
+        Debug.Log("Stopping Wall Run" + Time.time);
+        rb.useGravity = true;
+        pm.wallrunning = false;
+        recentlyDetached = true;
+    }
+
+    private void WallJump() {
+        exitingWall = true;
+        exitWallTimer = exitWallTime;
+
+        Vector3 wallNormal = wallRight ? rightWallHit.normal : leftWallHit.normal;
+
+        Vector3 forceToApply = transform.up * wallJumpUpForce + wallNormal * wallJumpSideForce;
+
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(forceToApply, ForceMode.Impulse);
+    }
+}
+
