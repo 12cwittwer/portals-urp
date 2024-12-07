348b86b7e3d37a81071c7003306991ee13feb22f
Christian Wittwer <christianwittwer@Christians-MacBook-Air.local>
2024-12-07 15:33:52 -0700

    Able to shoot portals

---
+++ Assets/Scripts/PortalPlacementWallrun.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerCam))]
+public class PortalPlacementWallrun : MonoBehaviour
+{
+    [SerializeField]
+    private PortalPair portals;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    [SerializeField]
+    private Crosshair crosshair;
+
+    [SerializeField]
+    private PlayerCam playerCam; // Reference to the PlayerCam script
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FirePortal(0, playerCam.camHolder.position, playerCam.camHolder.forward, 250.0f);
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            FirePortal(1, playerCam.camHolder.position, playerCam.camHolder.forward, 250.0f);
+        }
+    }
+
+    private void FirePortal(int portalID, Vector3 pos, Vector3 dir, float distance)
+    {
+        RaycastHit hit;
+        Physics.Raycast(pos, dir, out hit, distance, layerMask);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Portal")
+            {
+                var inPortal = hit.collider.GetComponent<Portal>();
+
+                if (inPortal == null)
+                {
+                    return;
+                }
+
+                var outPortal = inPortal.OtherPortal;
+
+                Vector3 relativePos = inPortal.transform.InverseTransformPoint(hit.point + dir);
+                relativePos = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativePos;
+                pos = outPortal.transform.TransformPoint(relativePos);
+
+                Vector3 relativeDir = inPortal.transform.InverseTransformDirection(dir);
+                relativeDir = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativeDir;
+                dir = outPortal.transform.TransformDirection(relativeDir);
+
+                distance -= Vector3.Distance(pos, hit.point);
+
+                FirePortal(portalID, pos, dir, distance);
+                return;
+            }
+
+            var cameraRotation = playerCam.camHolder.rotation; // Use PlayerCam rotation
+            var portalRight = cameraRotation * Vector3.right;
+
+
+            if (Mathf.Abs(portalRight.x) >= Mathf.Abs(portalRight.z))
+            {
+                portalRight = (portalRight.x >= 0) ? Vector3.right : -Vector3.right;
+            }
+            else
+            {
+                portalRight = (portalRight.z >= 0) ? Vector3.forward : -Vector3.forward;
+            }
+
+            var portalForward = -hit.normal;
+            var portalUp = -Vector3.Cross(portalRight, portalForward);
+
+            var portalRotation = Quaternion.LookRotation(portalForward, portalUp);
+
+            bool wasPlaced = portals.Portals[portalID].PlacePortal(hit.collider, hit.point, portalRotation);
+
+            if (wasPlaced)
+            {
+                crosshair.SetPortalPlaced(portalID, true);
+            }
+        }
+    }
+}
